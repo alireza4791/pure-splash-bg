5a585a60645fe4348f98d7c29a3c563e2f72ec0e
netherygo <overlord479@gmail.com>
2024-12-02 21:55:46 +0330

    .

--- index.js
+++ index.js
@@ -597,14 +597,19 @@ const showSplat = () => {
     let lastTime = Date.now();
     Update();
 
+    function drawColor(color) {
+        displayProgram.bind();
+        // splatProgram.bind();
+        gl.uniform4f(displayProgram.uniforms.color, 1, 1, 1, 1);
+        // gl.uniform4f(splatProgram.uniforms.color, 1, 1, 1, 1);
+        // blit(target);
+    }
+
     function Update() {
         resizeCanvas();
-
         const dt = Math.min((Date.now() - lastTime) / 1000, 0.016);
         lastTime = Date.now();
-
         gl.viewport(0, 0, TEXTURE_WIDTH, TEXTURE_HEIGHT);
-
         advectionProgram.bind();
         gl.uniform2f(
             advectionProgram.uniforms.texelSize,
@@ -687,7 +692,7 @@ const showSplat = () => {
         gl.uniform1i(gradienSubtractProgram.uniforms.uVelocity, velocity.first[2]);
         blit(velocity.second[1]);
         velocity.swap();
-
+        drawColor(1, 1, 1, 1);
         gl.viewport(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight);
         displayProgram.bind();
         gl.uniform1i(displayProgram.uniforms.uTexture, density.first[2]);
